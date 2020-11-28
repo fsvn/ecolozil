04795300afedfea3d0c8bc225ea4949e74795a9d
unknown <>
2020-11-28 20:18:04 +0400

    Initial commit

---
+++ client/components/LoginForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <form method="post">
+    <h1>Login</h1>
+    <input type="text" name="email" placeholder="E-mail" required/>
+    <input type="password" name="password" placeholder="Password" required/>
+    <input type="submit" value="Login" />
+  </form>
+</template>
+
+<style>
+
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  max-width: 500px;
+  min-height: 100vh;
+}
+</style>
