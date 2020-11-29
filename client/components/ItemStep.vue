d3477d6be030b4b7842d5d23d396da0be60529c4
sam-bhav03 <sam.buzz03@outlook.com>
2020-11-29 23:55:25 +0400

    yes commit

--- client/components/ItemStep.vue
+++ client/components/ItemStep.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <p>Met man prodwi ki pe vande dan ou kadi.</p>
+    <p>Met man prodwiii ki pe vande dan ou kadi.</p>
+
+
+    <div class="weather-test">
+    <p>enfin pfff.</p>
+
+  <div class="container searchContainer">
+            <div class="search card card-body">
+                <h1>check letemps dehors 1 kou</h1>
+                <p class="lead">met ou l'adresss</p>
+                <input type="text" id="searchUser" class="form-control" placeholder="Location">
+            </div>
+            <br>
+            <div id="profile"></div>
+        </div>
+        <div class="container text-center mt-2">
+            <button class="btn btn-primary" id="submit">confirmer</button>
+        </div>
+
+    </div>
 
     <div class="item-table" v-if="noItems > 0">
       <ItemStepCard
---
+++ client/components/Weather.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>Met man prodwi kiii pe vande dan ou kadi.</p>
+
+     
+  </div>
+</template>
+ 
