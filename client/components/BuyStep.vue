807a7ee33a9402a98634a89a8a7fd15568f2606d
unknown <>
2020-11-29 21:42:04 +0400

    Tweak color

--- client/components/BuyStep.vue
+++ client/components/BuyStep.vue
@@ -26,8 +26,8 @@ input {
   box-sizing: border-box;
   width: 100%;
   padding: 0.8em 0.5em;
-  margin: 0.2em 0em;
-  border: 1px solid rgb(100, 100, 100);
+  margin: 0.4em 0em;
+  border: 1px solid rgb(150, 150, 150);
   border-radius: 0.5em;
 }
 
