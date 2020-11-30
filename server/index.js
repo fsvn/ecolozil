e0171625ba4616749ead7ddc79cbff7b0ce2525e
unknown <>
2020-11-30 11:35:09 +0400

    Implement api/items

--- server/index.js
+++ server/index.js
@@ -1,49 +1,78 @@
-import mongodb from 'mongodb';
+import mongoose from 'mongoose';
 import express from 'express';
+//import bodyParser from 'body-parser';
 
-const { MongoClient } = mongodb;
-
+// Application instance.
 const app = express();
-const port = 3080;
+// Parse POST data.
+//app.use(bodyParser);
 
 // Application configuration.
 const config = {
-	port: 3080,
-	db: {
-		uri: 'mongodb://localhost/'
-	}
+  port: 3080,
+  db: {
+    uri: 'mongodb://localhost/ebazzar'
+  }
 };
 
-const client = new MongoClient(config.db.uri);
-
-await client.connect();
-
-console.log('MongoDb client connected');
-
-// RESTful API surface.
-app.post('/api/login', (req, res) => {
-	res.send('TODO: Implement.');
+// Create default mongodb connection.
+mongoose.connect(config.db.uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-app.post('/api/register', (req, res) => {
-	res.send('TODO: Implement.');
+mongoose.connection.on('error', e => {
+  console.error(`Failed to connect to mongodb instance using ${config.db.uri}`);
+  console.error(e);
+})
+
+// Our models.
+const Item = mongoose.model('Item', {
+  name: String,
+  image: String,
+  price: Number
 });
 
-app.get('/api/items', (req, res) => {
-	res.json([
-		{
-			name: 'bred',
-			price: 20
-		},
-		{
-			name: 'pome de terre',
-			price: 20
-		},
-		{
-			name: 'the thing is',
-			price: 30
-		}
-	]);
+const Order = mongoose.model('Order', {
+  firstname: String,
+  lastname: String,
+  phone: Number
 });
 
-app.listen(port, () => console.log(`Listenting on port ${port}...`));
+// Run this to set up intially.
+function setup() {
+  Item.insertMany([
+    {
+      name: 'Tomat',
+      price: 20
+    },
+    {
+      name: 'Pomedeter',
+      price: 30
+    },
+    {
+      name: 'Karot',
+      price: 20
+    }
+  ])
+}
+
+app.get('/api/items', async (req, res) => {
+  let items = {};
+  for (let item of  await Item.find())
+    items[item._id] = item;
+  res.json(items);
+});
+
+app.post('/api/order', async (req, res) => {
+  await Order.insertMany({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    phone: req.body.phone
+  });
+
+  // TODO: Figure out if we're handling this properly.
+  res.send();
+});
+
+app.listen(config.port, () => console.log(`Listenting on port ${config.port}...`));
