00a9a4bef27a654c995c08eef4c56bb5c98ab923
unknown <>
2020-11-30 20:57:40 +0400

    Implement ready state

--- client/components/steps/StepSection.vue
+++ client/components/steps/StepSection.vue
@@ -1,29 +1,43 @@
 <template>
   <section>
-    <h1><span>{{ count }}</span>{{ title }}</h1>
-    <slot></slot>
+    <h1>
+      <span :class="{ ready: isReady }">{{ count }}</span>
+
+      {{ title }}
+    </h1>
+
+    <slot />
   </section>
 </template>
 
 <style scoped>
 span {
   display: inline-block;
-  color: white;
-  background: rgb(147, 211, 147);
+  border: solid 1px rgb(147, 211, 147);
   border-radius: 100%;
   width: 1.5em;
   height: 1.5em;
   text-align: center;
-  margin-right: 0.8em;
+  margin-right: 0.4em;
+  transition: 128ms;
 }
 
 section {
   padding: 1em 0;
 }
+
+.ready {
+  color: white;
+  background: rgb(147, 211, 147);
+}
 </style>
 
 <script>
 export default {
-  props: ['count', 'title']
+  props: {
+    count: Number,
+    title: String,
+    isReady: Boolean
+  }
 }
 </script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <StepSection count="1" title="Shopping">
+    <StepSection :count=1 title="Shopping" :is-ready="shoppingReady">
       <ShoppingStep />
     </StepSection>
 
-    <StepSection count="2" title="Lokasion">
+    <StepSection :count=2 title="Lokasion" :is-ready="locationReady">
       <LocationStep />
     </StepSection>
 
-    <StepSection count="3" title="Aste">
+    <StepSection :count=3 title="Aste" :is-ready="buyReady">
       <BuyStep />
     </StepSection>
   </div>
@@ -16,7 +16,20 @@
 
 <script>
 export default {
+  computed: {
+    shoppingReady: function() {
+      return Object.keys(this.$store.state.cart).length > 0;
+    },
+    locationReady: function() {
+      return this.$store.state.marker != null;
+    },
+    buyReady: function() {
+      // TODO: Implement.
+      return false;
+    }
+  },
   async created() {
+    // Try to fetch data from the database instance.
     try {
       this.$store.commit('setItems', await this.$axios.$get('/api/items'));
     } catch (error) {
